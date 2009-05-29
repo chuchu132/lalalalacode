8ed6d4caab4f90e99dcd4a5c656117ed51f6c041
lu.teddy <lu.teddy@5b9d82fc-4657-11de-9eaa-61f86764e705>
2009-05-29 05:00:36 +0000

    version basica de la ventana.. no hace nada.. solo imprime por pantalla cuando clickean los botoncitos

---
+++ src/Ventana.h
@@ -0,0 +1,65 @@
+/*
+ * Ventana.h
+ *
+ *  Created on: 28/05/2009
+ *      Author: teddy
+ */
+
+#ifndef VENTANA_H_
+#define VENTANA_H_
+
+#include <gtkmm.h>
+#include <glib.h>
+
+#include <iostream>
+
+class Ventana {
+
+private:
+	Glib::RefPtr<Gtk::Builder> builder;//builder que hace referencia al archivo .glade
+	Gtk::Window *main_window; //ventana principal de la aplicacion
+	Gtk::AboutDialog* about_window; //ventana acerca de
+
+
+	/* botones */
+	Gtk::ToolButton *button_add;
+	Gtk::ToolButton *button_erase;
+	Gtk::ToolButton *button_stop;
+	Gtk::ToolButton *button_pause;
+	Gtk::ToolButton *button_continue;
+	Gtk::ToolButton *button_up;
+	Gtk::ToolButton *button_down;
+
+	//Gtk::MenuItem *menu_help;
+
+
+protected:
+  //Signal handlers:
+	void on_button_add_clicked();
+  	void on_button_erase_clicked();
+	void on_button_stop_clicked();
+	void on_button_pause_clicked();
+	void on_button_continue_clicked();
+	void on_button_up_clicked();
+	void on_button_down_clicked();
+
+	void on_menu_about();
+
+
+
+public:
+	Ventana();
+	~Ventana();
+
+	Gtk::Window& getVentana();
+
+private:
+	//obtiene los botones desde el archivo
+	void getButtons();
+
+	//conecta las señales con los signal handlers
+	void connectSignals();
+
+};
+
+#endif /* VENTANA_H_ */
